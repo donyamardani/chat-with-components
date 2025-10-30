5973d0579f082f1eeb68a7f62ecbbb86cd9ffec9
donyamardani <zmardani0@gmail.com>
2025-10-31 02:41:02 +0330

    add header component code
    add header component code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Header() {
+export default function Header({isType}) {
+  const [title,setTitle]=useState()
+  useEffect(()=>{
+    setTitle(isType==="send"?"chat client" : "chat support")
+  },[isType])
   return (
    
        <div className="panel-heading">
          <h3 className="panel-title">
-           <i className="icon wb-chat-text" aria-hidden="true"></i> Chat
+           <i className="icon wb-chat-text" aria-hidden="true"></i>  {title}
          </h3>
        </div>
   )
