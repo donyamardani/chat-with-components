e49aa024078999f775391ed1a09ff3ae5e63ca04
donyamardani <zmardani0@gmail.com>
2025-10-31 18:12:11 +0330

    add footer component codes
    add footer component codes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Footer() {
+export default function Footer({handleMessage,isType}) {
+  const[inp,setInp]=useState();
+ const handleSubmit=(e)=>{
+  e.preventDefault()
+  handleMessage(inp,isType)
+  setInp("")
+ };
   return (
    
     <div className="panel-footer">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="input-group">
-          <input type="text" className="form-control" placeholder="Say something"/>
+          <input value={inp} type="text" className="form-control" placeholder="Say something" onChange={e=>setInp(e?.target.value)}/>
           <span className="input-group-btn">
-            <button className="btn btn-primary" type="button">Send</button>
+            <button className="btn btn-primary" type="submit" disabled={!inp}>Send</button>
           </span>
         </div>
       </form>
