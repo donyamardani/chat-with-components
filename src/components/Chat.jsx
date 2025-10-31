31a4dc9d3907aa87de64c1cb62da43682e59ebf2
donyamardani <zmardani0@gmail.com>
2025-10-31 18:22:58 +0330

    add chat component codes
    add chat component codes

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 
-export default function Chat() {
+export default function Chat({avatars,message,isleft,time}) {
   return (
   
-        <div className="chat">
+        <div className={`chat ${isleft && 'chat-left'}`}>
           <div className="chat-avatar">
             <a className="avatar avatar-online" data-toggle="tooltip" href="#" data-placement="right" title="" data-original-title="June Lane">
-              <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="..."/>
+              <img src={avatars} alt="..."/>
               <i></i>
             </a>
           </div>
           <div className="chat-body">
             <div className="chat-content">
               <p>
-                Good morning, sir.
-                <br/>What can I do for you?
+                {message}
               </p>
-              <time className="chat-time" datetime="2015-07-01T11:37">11:37:08 am</time>
+              <time className="chat-time" datetime="2015-07-01T11:37">{time}</time>
             </div>
           </div>
         </div>
