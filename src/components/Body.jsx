59aad368c4b7ada974c811d66d57702edd89f7d2
donyamardani <zmardani0@gmail.com>
2025-10-31 17:56:32 +0330

    add body component codes
    add body component codes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Chat from './components/Chat'
 
-export default function Body() {
+export default function Body(messages,avatars,isType) {
+ const items= messages.map((msg,index)=>{
+    const isOwnmessage=msg.type===isType;
+    return(
+      <Chat
+      key={index}
+      avatars={msg.type='send'?avatars.client:avatars.support}
+      message={msg.message}
+      isleft={!isOwnmessage}
+      time={msg.time}
+      />
+    )
+  })
   return (
     
     <div className="panel-body">
       <div className="chats">
-        <Chat/>
+        {items}
       </div>
     </div>
   )
